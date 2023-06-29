84713b8974cd076adb708986d19e4ba7af0c17ae
zoznoor23 <115984329+zoznoor23@users.noreply.github.com>
2023-06-29 16:55:30 +0300

    replace grpcurl

--- tools.sh
+++ tools.sh
@@ -10,6 +10,6 @@ sudo apt install -y ./code_1.79.2-1686734195_amd64.deb ./stegseek_0.6-1.deb ./su
 sudo apt install -y remmina  stegsnow redis nodejs ltrace strace ghidra feroxbuster stegsnow apktool jadx ghex arjun golang-go docker.io gobuster
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 sudo gem install zsteg
-go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
+go install github.com/fullstorydev/grpcui/cmd/grpcui@latest
 pip3 install xortool
 pip3 install flask-unsign
